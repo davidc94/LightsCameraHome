9e8fd65b87548dc29c5f082cc590bddf417e8527
davidc94 <davidc94@btinternet.com>
2017-03-09 13:27:19 +0000

    LibVLC Camera
    Network camera working via LibVLC, compiled with older version of
    library to use older tutorial. Also removed redundant classes and code.

--- app/src/main/java/brunel/fyp/david/lightscamerahome/CamerasFragment.java
+++ app/src/main/java/brunel/fyp/david/lightscamerahome/CamerasFragment.java
@@ -1,26 +1,15 @@
 package brunel.fyp.david.lightscamerahome;
 
 import android.content.Intent;
-import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import java.io.File;
 
-import brunel.fyp.david.lightscamerahome.libvlc.MainVideoActivity;
-
-import brunel.fyp.david.lightscamerahome.libvlc.DirectoryAdapter;
-import brunel.fyp.david.lightscamerahome.libvlc.LibVLC;
-import brunel.fyp.david.lightscamerahome.libvlc.LibVlcException;
-import brunel.fyp.david.lightscamerahome.libvlc.MediaList;
-import brunel.fyp.david.lightscamerahome.libvlc.VideoActivity;
 
 /**
  * Created by David-Desktop on 16/02/2017.
@@ -32,9 +21,6 @@ public class CamerasFragment extends Fragment{
     private String userName = "root";
     private String password = "Axis206";
     private String streamAddress = "http://root:Axis206@192.168.1.112/mjpg/video.mjpg";
-
-    DirectoryAdapter mAdapter;
-    LibVLC mLibVLC;
 
     Button button1;
     Button button2;
@@ -57,8 +43,8 @@ public class CamerasFragment extends Fragment{
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), VideoActivity.class);
-                        intent.putExtra(VideoActivity.LOCATION, streamAddress);
+                Intent intent = new Intent(getActivity(), VLCActivity.class);
+                        intent.putExtra("videoUrl", streamAddress);
                         startActivity(intent);
             }
         });
